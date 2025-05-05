c79f8bdc0ee3e69412eda772c40fee762e9b360d
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-05-05 19:46:44 +0200

    feat: add session status prints

--- src/timer/sessionizer.rs
+++ src/timer/sessionizer.rs
@@ -2,47 +2,56 @@
 #[derive(Debug, Eq, PartialEq)]
 pub enum SessionState {
     Focus,
-    Break, 
+    Break,
 }
 
 // Keeps track of session information
 #[derive(Debug)]
 pub struct Session {
     pub rounds: u64,
-    pub state : SessionState,
+    pub state: SessionState,
 }
 
 impl Session {
     // Creates and retruns a new session object
-    pub fn new(rounds : u64) -> Self {
+    pub fn new(rounds: u64) -> Self {
         Self {
             rounds,
             state: SessionState::Focus,
         }
     }
 
-    // Advances the status of the session
+    // Advances the status of the session, returns whether or not it is
+    // possible to advance.
     pub fn advance(&mut self) -> bool {
         self.rounds -= 1;
 
         if self.rounds <= 0 {
-            return false
+            println!("You are done! Good job!");
+            return false;
         }
         self.flip_state();
-        
-        return true
+        println!("Time for {}", self.get_state_as_string());
+        return true;
     }
-    
+
     // TODO: Refactor to toggle // Toggles the state of the session
-    pub fn flip_state(&mut self){
+    pub fn flip_state(&mut self) {
         if self.state == SessionState::Focus {
-                self.state = SessionState::Break;
-                return
-        }
-        else {
+            self.state = SessionState::Break;
+            return;
+        } else {
             self.state = SessionState::Focus;
-            return
+            return;
         }
     }
 
+    fn get_state_as_string(&self) -> String {
+        match self.state {
+            SessionState::Focus => return String::from("Focus"),
+            SessionState::Break => return String::from("Break"),
+            // Throw error here later
+            //_ => return String::from("NOTHING"),
+        }
+    }
 }
