71aa9283c777d45d65d16e7adaf5197b192b4588
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-05-07 20:40:04 +0200

    feat: add prints for progression

--- src/timer/mod.rs
+++ src/timer/mod.rs
@@ -5,24 +5,26 @@ pub use sessionizer::Session;
 pub use sessionizer::SessionState;
 pub use stopwatch::Stopwatch;
 
+
 pub fn run(focus: u64, break_: u64, rounds: u64) {
     let mut session = Session::new(rounds);
 
     loop {
-        println!("{}", session.rounds.to_string());
+        println!("🦀🍅: Time for {} in round {}!", session.get_state_as_string(), session.rounds.to_string());
         match session.state {
             SessionState::Focus => {
                 let sw = Stopwatch::new(focus);
                 sw.start();
-                session.advance();
+                session.flip_state();
             }
             SessionState::Break => {
-                if !session.check_done() {
+                if session.check_done() {
+                    println!("🦀🍅: You are done! Good job!");
                     break;
                 }
                 let sw = Stopwatch::new(break_);
                 sw.start();
-                session.flip_state();
+                session.advance();
             }
         }
     }
